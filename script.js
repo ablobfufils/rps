b547864f553601513f2c1e38c18043b5b23825a2
ablobfufils <coltermd@gmail.com>
2020-05-01 09:16:20 -0300

    rewrote random rps function

--- script.js
+++ script.js
@@ -1,7 +1,2 @@
-const rps = ["Rock", "Paper", "Scissors"];
-const rpsRando = rps[Math.floor(Math.random() * rps.length)];
-
-let rpsRock = document.getElementById("insertRpsIDHere").value;
-let rpsPaper = document.getElementById("insertRpsIDHere").value;
-let rpsScissors = document.getElementById("insertRpsIDHere").value;
+document.getElementById("rock").onclick = "hello";
 
