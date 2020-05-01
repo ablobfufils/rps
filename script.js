4e6afcc76db6eae98a4752cbebe72972a8c3567e
ablobfufils <coltermd@gmail.com>
2020-04-30 21:45:13 -0300

    added base code to script.js

---
+++ script.js
@@ -0,0 +1,7 @@
+const rps = ["Rock", "Paper", "Scissors"];
+const rpsRando = rps[Math.floor(Math.random() * rps.length)];
+
+let rpsRock = document.getElementById("insertRpsIDHere").value;
+let rpsPaper = document.getElementById("insertRpsIDHere").value;
+let rpsScissors = document.getElementById("insertRpsIDHere").value;
+
